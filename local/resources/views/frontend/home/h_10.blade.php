4bc6426b1b2ad65132a1dc6d78c456f9170f9070
nnduyquang@gmail.com <bunvodich>
2018-08-22 16:28:46 +0700

    ok

--- local/resources/views/frontend/home/h_10.blade.php
+++ local/resources/views/frontend/home/h_10.blade.php
@@ -1,6 +1,6 @@
 <div id="h_10">
     <h3 class="title">
-        GÓI CHỤP ẢNH CƯỚI TẠI SEA WEDDING
+        GÓI CHỤP HOT TẠI SEA WEDDING
     </h3>
     <div class="container">
         <div class="packet-price-carousel owl-carousel owl-theme">
@@ -11,7 +11,7 @@
                 <div class="content">
                     <h4><a href="#">Gói chụp Đà Nẵng - Hội An</a></h4>
                     <div class="description">
-                        Đà Nẵng vừa có núi non, vừa có biển xanh trải dài thơ mộng là địa điểm đáng đến một lần trong đời.
+                        Hội An như một bức tranh mộc mạc, giản dị và nên thơ, dù là ngày hay đêm, nơi đây vẫn mang trong mình những vẻ đẹp lôi cuốn lãng mạn.
                     </div>
                 </div>
             </div>
@@ -22,7 +22,7 @@
                 <div class="content">
                     <h4><a href="#">Gói chụp Đà Nẵng - Bà Nà</a></h4>
                     <div class="description">
-                        Đà Nẵng vừa có núi non, vừa có biển xanh trải dài thơ mộng là địa điểm đáng đến một lần trong đời.
+                        Làng Pháp tại khu du lịch Bà Nà Hills tái hiện một nước Pháp cổ điển và lãng mạn với tổ hợp công trình kiến trúc độc đáo
                     </div>
                 </div>
             </div>
@@ -33,7 +33,7 @@
                 <div class="content">
                     <h4><a href="#">Gói chụp Đà Nẵng - Đồi Chè</a></h4>
                     <div class="description">
-                        Đà Nẵng vừa có núi non, vừa có biển xanh trải dài thơ mộng là địa điểm đáng đến một lần trong đời.
+                        Đồi chè Đông Giang là một trong những địa điểm du lịch Đà Nẵng hấp dẫn dành cho các cặp đôi lưu lại kỷ niệm đẹp xanh mát...
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                 <div class="content">
                     <h4><a href="#">Gói Ưu Đãi Hấp Dẫn</a></h4>
                     <div class="description">
-                        Đà Nẵng vừa có núi non, vừa có biển xanh trải dài thơ mộng là địa điểm đáng đến một lần trong đời.
+                        Duy nhất trong 20 ngày, các CDCR đăng ký trọn gói chỉ với 6.500.000 với nhiều phần quà háp dẫn.
                     </div>
                 </div>
             </div>
