b37c0a190dd059bd9e56e33bf21c05202b532dee
nnduyquang@gmail.com <bunvodich>
2018-08-29 11:11:42 +0700

    ok

---
+++ local/resources/views/mail/price-to-me.blade.php
@@ -0,0 +1,6 @@
+<h1>Tên Của Tôi là {{$name}}</h1><br>
+<h1>Email Của Tôi là {{$email}}</h1><br>
+<h1>Điện Thoại Của Tôi là {{$phone}}</h1><br>
+<h1>Gói chụp: {{$content}}</h1>
+<h1>Nội Dung Của Tôi là {{$contentMail}}</h1><br>
+Thanks,<br>
