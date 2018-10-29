ca0b955a5703c58318efa931e8059a6292ae317a
nnduyquang@gmail.com <bunvodich>
2018-10-29 17:24:48 +0700

    ok

--- local/resources/views/frontend/home/h_1.blade.php
+++ local/resources/views/frontend/home/h_1.blade.php
@@ -33,7 +33,7 @@
                 <div class="info-group">
                     <i class="far fa-heart"></i>
                     <div class="info">
-                        gói ưu đãi hấp dẫn<br> <span> đà nẵng - bà nà</span> <br>chỉ với 6.500.000
+                        gói ưu đãi hấp dẫn<br> <span> đà nẵng - hội an</span> <br>chỉ với 7.900.000
                     </div>
                 </div>
             </div>
